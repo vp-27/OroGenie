a9a79d9f93cf99c6e2cbada6ca31c5c5670faa11
Vandan Patel <pvandant@gmail.com>
2024-10-13 12:18:33 -0400

    using chromedriver instead

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:5001/',  // Your Flask backend URL
+  // baseURL: 'http://127.0.0.1:5001/',  
+  baseURL: 'https://orogenie.onrender.com',
   timeout: 30000,
 });
 
