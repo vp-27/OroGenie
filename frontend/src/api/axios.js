6d424ca0c876dee09b6492efec70b5cbdd7b5215
Vandan Patel <pvandant@gmail.com>
2024-10-13 13:35:50 -0400

    revert

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'http://127.0.0.1:5001/',  
-  baseURL: 'https://orogenie.onrender.com',
+  baseURL: 'http://127.0.0.1:5001/',  
+  // baseURL: 'https://orogenie.onrender.com',
   timeout: 30000,
 });
 
